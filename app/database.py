d4ba0a3f2c64536860ebc06675a86fa348336812
GEORGE MUTALE <148863672+GEORGEMUTALE@users.noreply.github.com>
2025-11-22 08:50:23 +0000

    database.py

--- app/database.py
+++ app/database.py
@@ -12,7 +12,7 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
     db = SessionLocal()
-        try:
-                yield db
-                    finally:
-                            db.close()
+    try:
+        yield db
+    finally:
+        db.close()
